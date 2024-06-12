8a6eece31d61054cbe19df0f00f28915f83d9769
Rodrigo Alvarez Lago <rodralva@c1r11s5.42madrid.com>
2024-06-12 14:48:27 +0200

    libft

---
+++ libft/ft_strtok.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtok.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodralva <rodralva@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 15:38:22 by rodralva          #+#    #+#             */
+/*   Updated: 2024/06/07 16:23:30 by rodralva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	skip_spaces(const char *delim, char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] && ft_strchr(delim, (int)str[i]))
+		i++;
+	return (i);
+}
+
+char	*ft_strtok(char *str, const char *delim)
+{
+		static char *last;
+
+		if (!str)
+			str = last;
+		if (!str)
+			return (NULL);
+		str += skip_spaces(delim, str);
+		if (!*str)
+		{
+			last = NULL;
+			return (NULL);
+		}
+		last = str;
+		while (*last && !ft_strchr(delim, (int)*last))
+			last++;
+		if (!*last)
+			last = NULL;
+		else
+		{
+			*last = 0;
+			last++;
+		}
+		return (str);
+}
