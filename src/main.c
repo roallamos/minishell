b8d46c2df2a2fd69ec44b1f4067a384acf41b951
Rodrigo Alvarez Lago <rodralva@c1r11s4.42madrid.com>
2024-06-22 15:43:10 +0200

    add history no añade los saltos de linea

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rodralva <rodralva@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 14:12:44 by migumore          #+#    #+#             */
-/*   Updated: 2024/06/22 15:41:57 by rodralva         ###   ########.fr       */
+/*   Updated: 2024/06/22 15:42:46 by rodralva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,6 @@ int	main(int argc, char *argv[], char *env[])
 	dup_env(&data, env);
 	data.path_in_env = ft_find_path(data.env);
 	data.path = ft_split(data.path_in_env, ':');
-	read_i	nput(&data);
+	nput(&data);
 	return (0);
 }
