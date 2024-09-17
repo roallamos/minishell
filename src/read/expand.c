5a84f7881176b3d301d50307c30128bd834ec803
Rodrigo Alvarez Lago <rodralva@c1r11s5.42madrid.com>
2024-09-17 11:07:29 +0200

    .

--- src/read/expand.c
+++ src/read/expand.c
@@ -1,5 +1,16 @@
 #include <minishell.h>
 
+void	set_quotes(char c, int *d_quote, int *s_quote)
+{
+	if (c == '\'' && !*s_quote && !*d_quote)
+		*s_quote = 1;
+	else if (c == '\'' && *s_quote)
+		*s_quote = 0;
+	if (c == '"' && !*d_quote && !*s_quote)
+		*d_quote = 1;
+	else if (c == '"' && *d_quote)
+		*d_quote = 0;
+}
 char	*ft_replace(char *var, char *value, char *args)
 {
 	int	i;
@@ -62,8 +73,8 @@ char	*expand_var(t_data *data, char *args)
  {
  	int i;
     int j;
-	char	d_quote;
-	char	s_quote;
+	int	d_quote;
+	int	s_quote;
 
  	i = 0;
     j = 0;
@@ -75,14 +86,15 @@ char	*expand_var(t_data *data, char *args)
 		{
 			if (args[i][j] == '\'' || args[i][j] == '"')
 			{
-				if (args[i][j] == '\'' && !s_quote && !d_quote)
+				set_quotes(args[i][j], &d_quote, &s_quote); // comprobar que esto funciona cuando migu haga lo del pipex
+				/*if (args[i][j] == '\'' && !s_quote && !d_quote)
 					s_quote = 1;
 				else if (args[i][j] == '\'' && s_quote)
 					s_quote = 0;
 				if (args[i][j] == '"' && !d_quote && !s_quote)
 					d_quote = 1;
 				else if (args[i][j] == '"' && d_quote)
-					d_quote = 0;
+					d_quote = 0;*/
 			}
 			else if (args[i][j] == '$' && !s_quote)
 				args[i] = expand_var(data, args[i]);
