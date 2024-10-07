d4f9252315736335537fea91f3c82ba9fcd74043
Rodrigo Alvarez <roallamos@gmail.com>
2024-10-07 14:17:43 +0200

    leaksç

--- src/executor/do_exit.c
+++ src/executor/do_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   do_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: migumore <migumore@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: rodralva <rodralva@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 19:14:44 by migumore          #+#    #+#             */
-/*   Updated: 2024/09/19 13:50:37 by migumore         ###   ########.fr       */
+/*   Updated: 2024/10/07 14:08:59 by rodralva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,10 @@ void	do_exit(t_data *data)
 
 	printf("exit\n");
 	exit_status = ft_atoi(data->list->args[1]);
+	free(data->prompt);
+	ft_free_array(data->env);
+	ft_free_array(data->path);
+	ft_free_lst(data);
 	if (exit_status != 0)
 		exit(exit_status);
 	else
